cc89de555668afd9c4fe2d2005b1e86a1b29f02b
neHko <dekart@abv.bg>
2021-02-02 01:19:05 +0200

    Delete area.py

--- area.py
+++
@@ -1,6 +0,0 @@
-area = float(input())
-price = (area * 7.61)
-final_price = (price -(price * 0.18))
-discount = (price * 0.18)
-print(f'The final price is: {final_price} lv.' )
-print(f'The discount is: {discount} lv.')
